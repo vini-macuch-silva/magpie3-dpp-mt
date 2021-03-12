b413fbdf7311dd3f650f3ee06d5e5c39ec7473e1
Marcel Klehr <mklehr@gmx.net>
2021-03-12 11:15:17 +0100

    Adjust magpie config

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
   experimentId: '56',
-  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
+  serverUrl: 'https://magpie-demo.herokuapp.com',
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=681A8813',
   contactEmail: 'michael.franke@uni-osnabrueck.de',
   mode: 'debug'
